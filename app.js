e8889f79a3bb720e61cf959d16281039bf767630
Thotholani-T <tembothotholani@gmail.com>
2022-12-30 13:54:16 +0200

    Added dynamic ports

--- app.js
+++ app.js
@@ -77,6 +77,6 @@ app.get("/watch", function (req, res) {
 	res.render("watch");
 });
 
-app.listen(3000, function () {
-	console.log("Server started on port 3000");
+app.listen(process.env.PORT || 3000, function () {
+	console.log("Server started on port " + process.env.PORT);
 });
